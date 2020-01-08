2571d374982f83a4a49210fd04c91fac3e61a712
lironrot <Lironrot@gmail.com>
2020-01-09 00:53:11 +0200

    Update Example.sh

--- Example.sh
+++ Example.sh
@@ -1,6 +1,18 @@
 #!/bin/bash 
 
-touch txt.txt
-cp txt.txt
-echo "insert massage here"
-bla bla bla bla
+#! /bin/bash
+
+NICLIST=$(cat /proc/net/dev | grep -e ":" | grep -v -e "lo"| cut -d ":" -f1)
+
+for i in $(echo $NICLIST) ;
+        do NICSTATUS=$(sudo ethtool $i 2>/dev/null | grep "Link detected" | cut -d : -f2) ;
+        if [ $NICSTATUS == "yes" ]
+        then
+          NICSTATUS="Status: active"
+        else
+          NICSTATUS="Status: not active"
+        fi;
+        NICSPEED=$(ethtool $i | grep -i speed);
+        NICIP=$(ip addr show $i | grep "inet\b" | awk '{print $2}' | cut -d/ -f1);
+        echo $i":" $NICIP , $NICSTATUS , $NICSPEED
+        done
